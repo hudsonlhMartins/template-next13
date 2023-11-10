50661c299d87da9b815684bc532aaaf7578e3695
hudson martins <hudsonlh.martins@gmail.com>
2023-11-09 22:14:03 -0300

    feat: finish template

---
+++ src/utils/tests/custom-render.ts
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+import { render, RenderOptions } from "@testing-library/react";
+
+const customRenderOptions = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+) => render(ui, { ...options });
+
+export * from "@testing-library/react";
+export { customRenderOptions as render };
